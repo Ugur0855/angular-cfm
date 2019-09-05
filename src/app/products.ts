d6f348295a0761a3c7b5bcdcb54b243bee9aeeb3
Ugur0855 <ugur_k.oglu@hotmail.com>
2019-09-05 15:05:41 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Mert Abi',
+    price: 5000,
+    description: 'Mert abinin açıklaması'
+  },
+  {
+    name: 'İlker Abi',
+    price: 1000,
+    description: 'İlker abinin açıklaması'
+  },
+  {
+    name: 'Altan Abi',
+    price: 2000,
+    description: 'Altan abinin açıklaması'
+  },
+  {
+    name: 'Önder Abi',
+    price: 1500,
+    description: 'Önder abinin açıklaması'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
